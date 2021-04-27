95fc4b50444cf33c28772984baf388cb807bb8ce
Paul Hanyzewski <paulhanyzewski@teamsnap.local>
2021-04-27 13:20:59 -0600

    do not install vscode extentsions, use auto backup sync

--- install.sh
+++ install.sh
@@ -154,13 +154,13 @@ mkdir -p ~/code/personal
 mkdir -p ~/code/src
 
 # Install extensions
-if command_does_not_exist code; then
-  error "Open VS Code and run: Install code command in \$PATH"
-else
-  for extension in $(cat vscode/extensions); do
-    code --install-extension "$extension"
-  done
-fi
+# if command_does_not_exist code; then
+#   error "Open VS Code and run: Install code command in \$PATH"
+# else
+#   for extension in $(cat vscode/extensions); do
+#     code --install-extension "$extension"
+#   done
+# fi
 
 # Repeated keys in VSCode _won't_ pop up the accent/umlaut menu
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
